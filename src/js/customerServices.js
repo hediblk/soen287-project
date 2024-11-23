3e10740e99a5ae921675b10aac9306f9386ba285
Hedi Belkahia <hedibelkahia03@gmail.com>
2024-11-23 14:46:20 -0500

    Implement services fetching form database on the admin services panel

--- src/js/customerServices.js
+++ src/js/customerServices.js
@@ -65,7 +65,6 @@ function resetButtons() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  //displayServices();
   resetButtons();
 });
 
--- src/js/landingPages.js
+++ src/js/landingPages.js
@@ -25,15 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
   })();
 });
 
-const storedServices = [
-  { name: "Newcomer Package", price: "25.00" },
-  { name: "Standard Package", price: "40.00" },
-  { name: "Experienced Package", price: "75.00" },
-  { name: "Deluxe Package", price: "100.00" },
-  { name: "VIP Deluxe Package", price: "150.00" },
-  { name: "King's Package", price: "250.00" },
-];
 
-if (!localStorage.getItem("servicesArray")) {
-  localStorage.setItem("servicesArray", JSON.stringify(storedServices));
-}
